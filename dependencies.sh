6ddbebb7ee49900e365c61db8ce0976e557bfe78
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2018-02-25 05:57:21 +0200

    Import rules
    Include the eslint-plugin-import in the autoinstalled
    dependencies & rules configuration for imports.
    
    Additionally, replace the flowtype plugin with flow.

---
+++ dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+npm install \
+  --save-dev \
+  eslint \
+  eslint-plugin-import \
+  eslint-plugin-react \
+  eslint-plugin-flow \
+  babel-eslint \
+  2> /dev/null
+
+exit 0
--- index.js
+++ index.js
@@ -12,6 +12,23 @@ const exceptMethods = [
   'componentWillReceiveProps',
 ]
 
+const globals = [
+  'Set',
+  'Map',
+  'Proxy',
+  'Symbol',
+  'Promise',
+  'WeakSet',
+  'WeakMap',
+  'Uint8Array',
+  '__DEV__',
+]
+
+const always = 'always'
+const never  = 'never'
+const W      = 'warn'
+const E      = 'error'
+
 module.exports = {
   parser: 'babel-eslint',
   env: {
@@ -22,7 +39,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'flowtype'
+    'flow',
+    'import',
   ],
   extends: [
     "plugin:flowtype/recommended",
@@ -35,45 +53,62 @@ module.exports = {
       jsx: true
     }
   },
-  globals: {
-    Set:        true,
-    Map:        true,
-    Proxy:      true,
-    Symbol:     true,
-    Promise:    true,
-    WeakSet:    true,
-    WeakMap:    true,
-    Uint8Array: true,
-  },
+  globals: globals.reduce((g, c) => Object.assign(g, { [c]: true }), {}),
   rules: {
     // Ignored
     strict: 0,
 
     // Warnings
-    semi:        [ 'warn', 'never' ],
-    complexity:  [ 'warn', 6 ],
-    'max-depth': [ 'warn', 3 ],
-    'max-lines': [ 'warn', 600 ],
-    'max-len':   [ 'warn', 120 ],
+    semi:        [ W, never ],
+    complexity:  [ W, 6 ],
+    'max-depth': [ W, 3 ],
+    'max-lines': [ W, 720 ],
+    'max-len':   [ W, 140 ],
 
-    'no-tabs':                'warn',
-    'no-console':             'warn',
-    'dot-notation':           'warn',
-    'class-methods-use-this': [ 'warn', { exceptMethods } ],
+    'no-tabs':                W,
+    'debugger':               W,
+    'no-console':             W,
+    'dot-notation':           W,
+    'class-methods-use-this': [ W, { exceptMethods } ],
 
     // Errors
-    eqeqeq:      [ 'error', 'smart' ],
-    'use-isnan':   'error',
+    eqeqeq:      [ E, 'smart' ],
+    'use-isnan':   E,
 
-    'no-undef':             'error',
-    'no-obj-calls':         'error',
-    'no-unused-vars':       'error',
-    'no-array-constructor': 'error',
+    'no-undef':             E,
+    'no-obj-calls':         E,
+    'no-unused-vars':       E,
+    'no-array-constructor': E,
+    'no-func-assign':       E,
+    'no-class-assign':       E,
+
+    'comma-spacing': [ W, {
+      before: false,
+      after: true,
+    }],
+    'array-bracket-spacing': [ always, { arraysInArrays: false }],
+    'object-curly-spacing':  [ always, { objectsInObjects: false }],
+
+    'import/no-mutable-exports':     E,
+    'import/prefer-default-export':  E,
+    'import/no-unassigned-import': [ E, { allow: [
+      '**/*.{le,c,sc}ss',
+      'babel-*',
+      'reactotron',
+    ]}],
+    'import/no-extraneous-dependencies': [ E, {
+      'devDependencies':  [
+        '**/*.{test,spec}.js',
+        '**/{test,spec}/*.js',
+        '**/*.{dev,develop,development}.js{x,}',
+        '**/{dev,develop,development}/*.js{x,}',
+      ]
+    }]
   },
   settings: {
     react: {
       pragma: 'React',
-      version: "16.0.0"
+      version: "16.3.0"
     },
     flowtype: {
       onlyFilesWithFlowAnnotation: false
